1ea03161fb2a111b91dfd09ce8b24feb3065e82e
Mark Winterbottom <mark@londonappdeveloper.com>
2021-04-09 14:59:46 +0100

    Update settings.py

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'l+2fr-rwd31cqz(vct^(!r(thlu#meb5_d0i8jslx8jpn5ltk-'
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = [
-    'ec2-18-130-20-217.eu-west-2.compute.amazonaws.com',
+    'ec2-18-212-138-237.compute-1.amazonaws.com',
     '127.0.0.1'
 ]
 
